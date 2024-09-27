34dd39b6f05efbf7314e75c26a0c6d36c924440e
unknown <dickensallinone@gmail.com>
2024-09-27 12:49:29 +0800

    pushed updated version and implement redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Authenticated from './Authenticated';
+// import Authenticated from './Authenticated';
 
+// import { Provider } from 'react-redux';
+
+import store from './store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-     <App /> 
-     {/* <Authenticated />  */}
-  </React.StrictMode>
+
+     <Provider store={store}>
+      <App /> 
+     </Provider>
+
+ 
 );
 
 
