b2f79dbd7ff9fd4cc92b2291bcc853d68611adaa
AlierT <tetsu.kala@gmail.com>
2024-09-28 17:33:23 +0800

    adding on changes on alier branch

--- src/pages/DashBoardCareGiver.js
+++ src/pages/DashBoardCareGiver.js
@@ -4,39 +4,8 @@ const DashBoardCareGiver = () => {
 
 return ( 
 <>
-<div className="ml-3 text-blue d-block">
-    <div>
-      <p className="font-weight-bold h1-font-size ">Welcome!</p>
-    </div>
-
-    <div className="mt-3">
-    <p className="h2-font-size">Assistant123</p>
-    </div>
-    
-    <div className="d-flex mt-5 align-items-center">
-        <div className="mr-5">
-            <p>Requests</p>
-        </div>
-
-        <div>
-            <p>See more
-            </p>
-        </div>
-        
-        <div className="ml-2">
-            
-        <span className="material-symbols-outlined icon-forward-arrow">
-                arrow_forward
-            </span>
-        </div>
-    </div>
-    
-    <div className="dashboard-cargiver-container mt-3">
-        <p>
-            asdsadsad
-        </p>
-    </div>
-</div>
+<Banner />
+<ProfileCard />
 </>
 );
 
